2cc23416c2fbdaa8406149ce79dcd2a28a3ba6c6
ermaccch <157322975+ermaccch@users.noreply.github.com>
2024-02-13 23:38:31 +0300

    demo4

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Ты очень мне нравишься❤️";
+  question.innerHTML = "Ты мне очень нравишься❤️ Мы обязательно встретимся, красивая девушка!";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
